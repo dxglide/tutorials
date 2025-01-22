095b906814a0b4bfe990e7eae9847ac7d4f505e0
Tomas Bandza <dxglide@gmail.com>
2025-01-22 09:47:29 +0200

    Compare

---
+++ compare.py
@@ -0,0 +1,12 @@
+print("Compare ...");
+print(20 > 21);
+print('bapple' < 'aorange');
+
+numeris = input("Irasyk numeri: ")
+if int(numeris) > 10:
+    print("Daugiua nei mano")
+elif int(numeris) == 10:
+    print("Lygu")
+else: 
+    print("maziau nei mano")
+
